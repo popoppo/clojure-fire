eb7e933b41572691f58196900700c2147ae6e8a5
Koji Takahashi <popoppo39@gmail.com>
2020-08-07 15:40:46 +0900

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,18 @@
-(defproject shoot "0.0.1"
+(defproject shoot "0.0.1-SNAPSHOT"
   :description "Automatic CLI generator"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"
-                  org.flatland/useful "0.11.6"]]
-  :repl-options {:init-ns shoot.core})
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.flatland/useful "0.11.6"]]
+  :repl-options {:init-ns shoot.core}
+
+
+  :plugins [[cider/cider-nrepl "0.25.2"]
+            [lein-exec "0.3.7"]
+            [lein-pprint "1.1.1"]
+            [refactor-nrepl "2.5.0"]]
+
+  :src-paths ["src"]
+  :test-paths ["test"]
+  )
