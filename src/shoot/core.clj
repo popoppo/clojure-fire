274993656de500d7cfc092ba2acd9753a5721c8f
Koji Takahashi <popoppo39@gmail.com>
2020-08-26 16:29:30 +0900

    Implement basic functionalites

--- src/shoot/core.clj
+++ src/shoot/core.clj
@@ -1,27 +1,12 @@
 (ns shoot.core
   (:require
    [clojure.edn :as edn]
-   [flatland.useful.cli :refer [parse-opts]]))
+   [clojure.string :as str]))
 
 (def default-parsers
-  [[#".+" #(clojure.edn/read-string %)]])
+  [[#".+" #(edn/read-string %)]])
 
-(defn apply-func
-  [func-name positional-args options]
-  (let [funcs-map (dissoc (ns-publics *ns*) 'shoot)
-        func-names (keep #(let [m (meta (second %))]
-                            (when (:arglists m)
-                              (:name m)))
-                         funcs-map)
-        args (if (seq options)
-               (concat positional-args
-                       (list options))
-               positional-args)]
-    (if (some #{func-name} func-names)
-      (apply (funcs-map func-name) args)
-      (println func-name "not found. expected one of" func-names))))
-
-(defn transform
+(defn- transform
   [pattern-list v]
   (loop [lst pattern-list]
     (cond
@@ -30,45 +15,143 @@
               (if (re-matches ptrn v)
                 (try
                   (f v) ;; transform
-                  (catch Exception ;; e (prn (.getMessage e))
+                  (catch Exception e ;; (prn (.getMessage e))
                     v)) ;; return str if transform fails
                 (recur (rest lst)))))))
+
+(defn cl-args-without-file-name
+  "As 'lein exec' keeps target file name in *command-line-args*,
+  first item in *command-line-args* has to be dropped"
+  []
+  (if (System/getenv "LEIN_JAVA_CMD")
+    (rest *command-line-args*)  ;; lein
+    *command-line-args*))
+
+(defn parse-value
+  [s]
+  {:pre [(string? s)]}
+  (condp #(re-matches %1 %2) s
+    #"-([a-zA-Z])" :>> #(hash-map (-> % second keyword) [])
+    #"--?([a-zA-Z][-\w]*)=(.+)" :>> #(hash-map (-> % second keyword) [(nth % 2)])
+    #"--?([a-zA-Z][-\w]*)" :>> #(hash-map (-> % second keyword) [])
+    {:* [s]}))
+
+(defn parse-arg-list
+  [arg-list]
+  (reduce
+   (fn [acc v] (->> (parse-value v)
+                    (merge-with into acc)))
+   {}
+   arg-list))
 
 (defn parse-command-line-args
   [args-has-func-name? & {:keys [custom-pattern-list]
                           :or {custom-pattern-list default-parsers}}]
-  (let [opts (parse-opts :pos (rest *command-line-args*)) ;; drop cmd
-        args (:pos opts)
-        f-symbol (when args-has-func-name?
-                   (symbol (first args)))
+  (let [cl-args (cl-args-without-file-name)
+        opts (parse-arg-list (if args-has-func-name?
+                               (rest cl-args)
+                               cl-args))
+        fn-symbol (when args-has-func-name?
+                    (symbol (first cl-args)))
         tf #(transform custom-pattern-list %)
-        tf-for-map #(let [[k vs] %]
-                      (hash-map k (map (fn [v] (tf v)) vs)))
-        positional-args (map tf (if args-has-func-name?
-                                  (rest args)
-                                  args))
-        options (map tf-for-map (dissoc opts :pos))]
-    {:func f-symbol
+        tf-for-map #(let [[k vs] %
+                          new-vs (map (fn [v] (tf v)) vs)]
+                      (hash-map k (condp = (count new-vs)
+                                    0 nil
+                                    1 (first new-vs)
+                                    new-vs)))
+        positional-args (map tf (:* opts))
+        options (->> (dissoc opts :*)
+                     (map tf-for-map)
+                     (apply merge {}))] ;; {} is to avoid nil
+    {:func fn-symbol
      :positional-args positional-args
      :options options}))
 
-(defn shoot
-  "This function is the main entrypoint."
+(defn get-publics
+  "Returns map of public vars.
+  With bb, use babashka.main (-m/--main optsion) or use *ns* (-f options).
+  With clj, parse command line args.
+  WIth lein-exec, just use *ns*"
+  []
+  (let [bb-main (System/getProperty "babashka.main")
+        cmd (System/getProperty "sun.java.command")
+        cmd-list (when cmd (str/split cmd #" +"))
+        target-ns (cond
+                    ;; bb -m
+                    bb-main (symbol bb-main)
+
+                    ;; bb -f or lein exec
+                    (or
+                     (System/getProperty "babashka.version")
+                     (System/getenv "LEIN_JAVA_CMD")) *ns* ;; lein
+
+                    ;; clj -m/--main
+                    :else
+                    (let [i (count *command-line-args*)
+                          run-cmd (drop-last i cmd-list)
+                          _ (prn run-cmd)
+                          idx-of-m (.indexOf run-cmd (some #{"-m" "--main"} run-cmd))]
+                      (symbol (nth run-cmd (inc idx-of-m)))))]
+    (-> target-ns
+        ns-publics
+        (dissoc 'shoot))))
+
+(defn print-func-names
+  "Print list of fn names and its doc"
+  []
+  (let [fns-map (get-publics)
+        fns-info (keep #(let [m (-> % second meta)]
+                          (when (:arglists m)
+                            (format "%s %s\n%s\n"
+                                    (:name m)
+                                    (:arglists m)
+                                    (or (:doc m) "No dostrings"))))
+                       fns-map)]
+    (println (str/join "\n" fns-info))))
+
+(defn apply-func
+  [func-name positional-args options]
+  (let [funcs-map (get-publics)
+        func-names (keep #(let [m (meta (second %))]
+                            (when (:arglists m)
+                              (:name m)))
+                         funcs-map)
+        args (if (seq options)
+               (concat positional-args (list options))
+               positional-args)]
+    (if (some #{func-name} func-names)
+      (apply (funcs-map func-name) args)
+      (println func-name "not found. Speccify one of" func-names))))
+
+(defn shoot*
+  "Main part of shoot."
   ([]
    (let [{:keys [func positional-args options]} (parse-command-line-args true)]
-     (apply-func func positional-args options)))
-  ([args]
-   (condp #(%1 %2) args
-     map? (let [custom-pattern-list (or (:parsers args) default-parsers)
-                cl-has-func-name? (if (:func args) false true)
+     (if (every? nil? [positional-args options])
+       (println "function-name must be specified")
+       (apply-func func positional-args options))))
+  ([arg]
+   (condp #(%1 %2) arg
+     map? (let [custom-pattern-list (or (:parsers arg) default-parsers)
+                cl-has-func-name? (if (:func arg) false true)
                 parsed (parse-command-line-args cl-has-func-name?
                                                 :custom-pattern-list custom-pattern-list)
                 {:keys [func positional-args options]} parsed
-                func-name (if func func (symbol (:func args)))]
-            (if (not func-name)
+                func-name (if func func (symbol (:func arg)))]
+            (if (nil? func-name)
               (println "function-name must be specified")
               (apply-func func-name positional-args options)))
      (let [{:keys [positional-args options]} (parse-command-line-args false)]
-       (apply-func (symbol args) positional-args options))))
+       (apply-func (symbol arg) positional-args options))))
   ([func-name parsers]
-   (shoot {:func func-name :parsers parsers})))
+   (shoot* {:func func-name :parsers parsers})))
+
+(defn shoot
+  "Entry point of shoot."
+  [& args]
+  (if (every? empty? [args (cl-args-without-file-name)])
+    (print-func-names)
+    (if (seq args)
+      (apply shoot* args)
+      (shoot*))))
--- test/shoot/core_test.clj
+++ test/shoot/core_test.clj
@@ -2,9 +2,7 @@
   (:require
    [clojure.edn :as edn]
    [clojure.test :refer [are deftest testing]]
-   [shoot.core :refer [shoot default-parsers]]))
-
-(def ^:private this-ns *ns*)
+   [shoot.core :refer [shoot default-parsers parse-arg-list parse-command-line-args] :as shoot]))
 
 (defn hello
   []
@@ -28,88 +26,153 @@
   (boolean v))
 
 (deftest shoot-test
-  (binding [*ns* this-ns]
-    (testing "With no args"
-      (are [orig args] (= orig
-                          (do
-                            (set! *command-line-args* args)
-                            (shoot)))
-        (hello) ["dummy-cmd" "hello"]
-        (hello-world) ["dummy-cmd" "hello-world"]
-        (hello-world :name "Clojure") ["dummy-cmd" "hello-world" ":name" "Clojure"]
-        (add 1 2) ["dummy-cmd" "add" "1" "2"]
-        (add 1 2 3 4 5) ["dummy-cmd" "add" "1" "2" "3" "4" "5"]
-        (volume {:x 2 :y 3 :z 4}) ["dummy-cmd" "volume" "{:x 2 :y 3 :z 4}"]
-        (is-that-true? true) ["dummy-cmd" "is-that-true?" "true"]
-        (is-that-true? false) ["dummy-cmd" "is-that-true?" "false"]
-        ;; edn/read-string is used to parse args and
-        ;; it parses "False" as string.
-        ;; As a result, (boolean "False") returns true.
-        (is-that-true? true) ["dummy-cmd" "is-that-true?" "False"]))
+  (binding [*ns* 'shoot.core-test]
+    (with-redefs [shoot.core/cl-args-without-file-name (fn [] *command-line-args*)]
+      (testing "With no args"
+        (are [orig args] (= orig
+                            (do
+                              (set! *command-line-args* args)
+                              (shoot)))
+          (hello) ["hello"]
+          (hello-world) ["hello-world"]
+          (hello-world :name "Clojure") ["hello-world" ":name" "Clojure"]
+          (add 1 2) ["add" "1" "2"]
+          (add 1 2 3 4 5) ["add" "1" "2" "3" "4" "5"]
+          (volume {:x 2 :y 3 :z 4}) ["volume" "{:x 2 :y 3 :z 4}"]
+          (is-that-true? true) ["is-that-true?" "true"]
+          (is-that-true? false) ["is-that-true?" "false"]
+          ;; edn/read-string is used to parse args and
+          ;; it parses "False" as string.
+          ;; As a result, (boolean "False") returns true.
+          (is-that-true? true) ["is-that-true?" "False"]))
 
-    (testing "With function name"
-      (are [orig func args] (= orig
-                               (do
-                                 (set! *command-line-args* args)
-                                 (shoot func)))
-        (hello) 'hello ["dummy-cmd"]  ;; (shoot 'hello)
-        (hello-world) "hello-world" ["dummy-cmd"] ;; str and keyword are also allowed
-        (hello-world :name "Clojure") :hello-world ["dummy-cmd" ":name" "Clojure"]
-        (add 1 2) 'add ["dummy-cmd" "1" "2"]
-        (add 1 2 3 4 5) 'add ["dummy-cmd" "1" "2" "3" "4" "5"]
-        (volume {:x 2 :y 3 :z 4}) 'volume ["dummy-cmd" "{:x 2 :y 3 :z 4}"]
-        (is-that-true? true) 'is-that-true? ["dummy-cmd" "true"]
-        (is-that-true? false) 'is-that-true? ["dummy-cmd" "false"]
-        (is-that-true? true) 'is-that-true? ["dummy-cmd" "False"]))
+      (testing "With function name"
+        (are [orig func args] (= orig
+                                 (do
+                                   (set! *command-line-args* args)
+                                   (shoot func)))
+          (hello) 'hello [] ;; (shoot 'hello)
+          (hello-world) "hello-world" [] ;; str and keyword are also allowed
+          (hello-world :name "Clojure") :hello-world [":name" "Clojure"]
+          (add 1 2) 'add ["1" "2"]
+          (add 1 2 3 4 5) 'add ["1" "2" "3" "4" "5"]
+          (volume {:x 2 :y 3 :z 4}) 'volume ["{:x 2 :y 3 :z 4}"]
+          (is-that-true? true) 'is-that-true? ["true"]
+          (is-that-true? false) 'is-that-true? ["false"]
+          (is-that-true? true) 'is-that-true? ["False"]))
 
-    (testing "With options"
-      (are [orig opts args] (= orig
-                               (do
-                                 (set! *command-line-args* args)
-                                 (shoot opts)))
-        (hello) {:func 'hello} ["dummy-cmd"]  ;; (shoot 'hello)
-        (hello-world) {:func 'hello-world} ["dummy-cmd"]
-        (hello-world :name "Clojure") {:func 'hello-world} ["dummy-cmd" ":name" "Clojure"]
-        (add 1 2) {:func 'add} ["dummy-cmd" "1" "2"]
-        (add 1 2 3 4 5) {:func 'add} ["dummy-cmd" "1" "2" "3" "4" "5"]
-        (volume {:x 2 :y 3 :z 4}) {:func 'volume} ["dummy-cmd" "{:x 2 :y 3 :z 4}"]
-        (is-that-true? true) {:func 'is-that-true?} ["dummy-cmd" "true"]
-        (is-that-true? false) {:func 'is-that-true?} ["dummy-cmd" "false"]
-        (is-that-true? true) {:func 'is-that-true?} ["dummy-cmd" "False"]))
+      (testing "With options"
+        (are [orig opts args] (= orig
+                                 (do
+                                   (set! *command-line-args* args)
+                                   (shoot opts)))
+          (hello) {:func 'hello} [] ;; (shoot 'hello)
+          (hello-world) {:func 'hello-world} []
+          (hello-world :name "Clojure") {:func 'hello-world} [":name" "Clojure"]
+          (add 1 2) {:func 'add} ["1" "2"]
+          (add 1 2 3 4 5) {:func 'add} ["1" "2" "3" "4" "5"]
+          (volume {:x 2 :y 3 :z 4}) {:func 'volume} ["{:x 2 :y 3 :z 4}"]
+          (is-that-true? true) {:func 'is-that-true?} ["true"]
+          (is-that-true? false) {:func 'is-that-true?} ["false"]
+          (is-that-true? true) {:func 'is-that-true?} ["False"]))
 
-    (testing "With custom parsers"
-      (let [enhanced-parsers (concat
-                              [[#"(?i)false" (fn [_] false)]]
-                              default-parsers)
-            wrong-order-parsers (concat
-                                 default-parsers  ;; this will catch all
-                                 [[#"(?i)false" (fn [_] false)]])
-            no-parsers []]
-        (testing "via args"
-          (are [orig parsers args] (= orig
-                                      (do
+      (testing "With custom parsers"
+        (let [enhanced-parsers (concat
+                                [[#"(?i)false" (fn [_] false)]]
+                                default-parsers)
+              wrong-order-parsers (concat
+                                   default-parsers ;; this will catch all
+                                   [[#"(?i)false" (fn [_] false)]])
+              no-parsers []]
+          (testing "via args"
+            (are [orig parsers args] (= orig
+                                        (do
+                                          (set! *command-line-args* args)
+                                          (shoot 'is-that-true? parsers)))
+              (is-that-true? false) enhanced-parsers ["False"]
+              (is-that-true? true) wrong-order-parsers ["False"]
+              ;; "false" is passed as string without parsers.
+              (is-that-true? true) no-parsers ["false"]))
+          (testing "via opts"
+            (are [orig parsers args] (= orig
+                                        (do
+                                          (set! *command-line-args* args)
+                                          (shoot {:func 'is-that-true?
+                                                  :parsers parsers})))
+              (is-that-true? false) enhanced-parsers ["False"]
+              (is-that-true? true) wrong-order-parsers ["False"]
+              (is-that-true? true) no-parsers ["false"]))))
+
+      (testing "Additional combinations"
+        (are [expected opts args] (= expected
+                                     (do
+                                       (set! *command-line-args* args)
+                                       (shoot opts)))
+          ;; With no parsers, :name is passed as plain string, not keyword.
+          "Hello World" {:parsers []} ["hello-world" ":name" "{:k 1}"]
+          ;; Only #":.+" is parsed as keyword, so "{...}" is handled as string
+          "Hello {:k 1}" {:parsers [[#":.+" #(edn/read-string %)]]} ["hello-world" ":name" "{:k 1}"])))))
+
+(deftest parse-arg-list-test
+  (testing "Positional args"
+    (are [expected args] (= expected (parse-arg-list args))
+      {} []
+      {:* [" "]} [" "] ;; " "
+      {:* ["1"]} ["1"]
+      {:* ["-1" "--1" "---1"]} ["-1" "--1" "---1"]
+      {:* ["\"abc\"" "def"]} ["\"abc\"" "def"]
+      {:* ["-_" "-?" "=" "==" "-="]} ["-_" "-?" "=" "==" "-="]
+      {:* ["\"abc\""]} ["\"abc\""] ;; "\"abc\"" or '"abc"'
+      ))
+  (testing "Options without values"
+    (are [expected args] (= expected (parse-arg-list args))
+      {:a []} ["-a"]
+      {:a []} ["--a"]
+      {:a [] :b []} ["-a" "-b"]
+      {:a [] :b []} ["-a" "-b" "-a"]))
+  (testing "Options with values"
+    (are [expected args] (= expected (parse-arg-list args))
+      {:a ["1"]} ["-a=1"]
+      {:a ["--a"]} ["--a=--a"]
+      {:a ["=="]} ["--a==="]
+      {:a ["-a=-a"]} ["-a=-a=-a"]
+      {:a ["1"] :b ["2"]} ["-a=1" "-b=2"]
+      {:a ["1" "3"] :b ["2"]} ["-a=1" "-b=2" "-a=3"]
+      {:A ["1,2,3"]} ["--A=1,2,3"] ;; will be string
+      {:A ["a b c"]} ["--A=a b c"] ;; --A="a b c" will be string
+      {:A ["[1 2 3]"]} ["--A=[1 2 3]"] ;; --A="[1 2 3]" will be vec
+      {:A ["(1 2 3)"]} ["--A=(1 2 3)"] ;; --A="(1 2 3)" will be list
+      {:A [","] :B ["1,"] :C [",,,"]} ["--A=," "--B=1," "--C=,,,"]
+      {:a ["   "]} ["--a=   "] ;; --a='   '
+      ))
+  (testing "Combined"
+    (are [expected args] (= expected (parse-arg-list args))
+      {:* ["-1"] :a ["1"]} ["-1" "-a=1"]
+      {:* ["--1" "-1"] :a ["1"]} ["--1" "-a=1" "-1"])))
+
+
+(deftest parse-command-line-args-test
+  (testing "Cl-args has fn name"
+    (with-redefs [shoot/cl-args-without-file-name (fn [] *command-line-args*)]
+     (are [expected args] (= expected (do
                                         (set! *command-line-args* args)
-                                        (shoot 'is-that-true? parsers)))
-            (is-that-true? false) enhanced-parsers ["dummy-cmd" "False"]
-            (is-that-true? true) wrong-order-parsers ["dummy-cmd" "False"]
-            ;; "false" is passed as string without parsers.
-            (is-that-true? true) no-parsers ["dummy-cmd" "false"]))
-        (testing "via opts"
-          (are [orig parsers args] (= orig
-                                      (do
-                                        (set! *command-line-args* args)
-                                        (shoot {:func 'is-that-true?
-                                                :parsers parsers})))
-            (is-that-true? false) enhanced-parsers ["dummy-cmd" "False"]
-            (is-that-true? true) wrong-order-parsers ["dummy-cmd" "False"]
-            (is-that-true? true) no-parsers ["dummy-cmd" "false"]))))
+                                        (parse-command-line-args false)))
+       {:func nil :positional-args '() :options {}} []
+       {:func nil :positional-args '(nil nil) :options {}} [" " "   "] ;; white spaces will be nill
+       {:func nil :positional-args '("   ") :options {}} ["\"   \""] ;; use '"' to pass spaces
+       {:func nil :positional-args '(a) :options {}} ["a"] ;; args will be symbol
+       {:func nil :positional-args '(abc "abc") :options {}} ["abc" "\"abc\""] ;; use '"' for str
+       {:func nil :positional-args '(-1 --1 ---1) :options {}} ["-1" "--1" "---1"]
+       {:func nil :positional-args '(:foo bar) :options {}} [":foo" "bar"]
+       {:func nil :positional-args '({:a 1} [1 2 3]) :options {}} ["{:a 1}" "[1 2 3]"]
+       {:func nil :positional-args '({:a {:b {:c 1}}}) :options {}} ["{:a {:b {:c 1}}}"]
+       {:func nil :positional-args '("},@_@,{") :options {}} ["},@_@,{"]
+       {:func nil :positional-args '(---c) :options {:a [] :b []}} ["-a" "--b" "---c"]
+       {:func nil :positional-args '() :options {:a 1 :b '(1 2 3) :c '(4 5 6)}} ["-a=1" "-b=[1 2 3]" "-c=(4 5 6)"]
+       {:func nil :positional-args '() :options {:a nil :b " "}} ["-a= " "--b=\" \""]
+       {:func nil :positional-args '() :options {:a '(1 3) :b '(2 [4 5 6])}} ["-a=1" "--b=2" "--a=3" "-b=[4 5 6]"]))))
 
-    (testing "Additional combinations"
-      (are [orig opts args] (= orig
-                               (do
-                                 (set! *command-line-args* args)
-                                 (shoot opts)))
-        ;; With no parsers, :name is passed as plain string, not keyword.
-        "Hello World" {:parsers []} ["dummy-cmd" "hello-world" ":name" "{:k 1}"]
-        ;; Only #":.+" is parsed as keyword, so "{...}" is handled as string
-        "Hello {:k 1}" {:parsers [[#":.+" #(edn/read-string %)]]} ["dummy-cmd" "hello-world" ":name" "{:k 1}"]))))
+(deftest simulating-commands-test
+  (testing "simulate bb")
+  (testing "simulate clj")
+  (testing "simulate lein-exec"))
