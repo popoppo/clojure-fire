b1f3c7c7145e2cbb7671aea3d587cf7f0e90b4f1
Koji Takahashi <popoppo39@gmail.com>
2020-08-27 09:22:17 +0900

    Update for GitHub Actions

--- project.clj
+++ project.clj
@@ -1,12 +1,15 @@
-(defproject shoot "0.0.1-SNAPSHOT"
-  :description "Automatic CLI generator"
-  :url "http://example.com/FIXME"
+(defproject shoot "0.0.1"
+  :description "Easy function dipatcher"
+  :url "https://github.com/popoppo/shoot"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.flatland/useful "0.11.6"]]
-  :repl-options {:init-ns shoot.core}
 
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+
+  :repositories [["releases" :clojars]
+                 ["snapshots" :clojars]]
+
+  :repl-options {:init-ns shoot.core}
 
   :plugins [[cider/cider-nrepl "0.25.2"]
             [lein-exec "0.3.7"]
@@ -14,5 +17,4 @@
             [refactor-nrepl "2.5.0"]]
 
   :src-paths ["src"]
-  :test-paths ["test"]
-  )
+  :test-paths ["test"])
