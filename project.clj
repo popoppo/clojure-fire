7108d382f14ced4db93b7ec4693c0841ccbba777
Koji Takahashi <popoppo39@gmail.com>
2020-08-27 12:01:22 +0900

    Update settings for clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject shoot "0.0.1"
+(defproject shoot "0.0.2-SNAPSHOT"
   :description "Easy function dipatcher"
   :url "https://github.com/popoppo/shoot"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -6,8 +6,10 @@
 
   :dependencies [[org.clojure/clojure "1.10.1"]]
 
-  :repositories [["releases" :clojars]
-                 ["snapshots" :clojars]]
+  :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
+                                  :username :env/clojars_username
+                                  :password :env/clojars_password
+                                  :sign-releases false}]]
 
   :repl-options {:init-ns shoot.core}
 
