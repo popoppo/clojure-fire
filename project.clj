c2eb7d088825da67f881a9d2b24cb05c10c3974e
Koji Takahashi <popoppo39@gmail.com>
2020-08-27 10:43:48 +0900

    Fix repository setting

--- project.clj
+++ project.clj
@@ -6,12 +6,10 @@
 
   :dependencies [[org.clojure/clojure "1.10.1"]]
 
-  :repositories [["clojars" {:url "https://clojars.org/repo"
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                              :username :env/clojars_username
                              :password :env/clojars_password
                              :sign-releases false}]]
-  :deploy-repositories [["releases" :clojars]
-                        ["snapshots" :clojars]]
 
   :repl-options {:init-ns shoot.core}
 
