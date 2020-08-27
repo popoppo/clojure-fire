9a56019db3d87e5fc5b17620a6352f8dfe592415
Koji Takahashi <popoppo39@gmail.com>
2020-08-27 11:41:54 +0900

    0.0.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject shoot "0.0.1"
+(defproject shoot "0.0.2-SNAPSHOT"
   :description "Easy function dipatcher"
   :url "https://github.com/popoppo/shoot"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
